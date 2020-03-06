8fcffe3d9b9778fc248c03311e82926559de799d
Susana <susymarques99@gmail.com>
2020-03-06 14:13:19 +0000

    first draft mainpage in xamarin

---
+++ TicketNow/TicketNow/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TicketNow
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void onLoginButtonClicked(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new Perfil());
+
+        }
+    }
+}
+
---
+++ TicketNow/TicketNow/Perfil.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace TicketNow
+{
+    public partial class Perfil : ContentPage
+    {
+        public Perfil()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
