d147709dbdafb1dd6bf50005c05f0c005d13ce48
KillByte <killbyte.games@gmail.com>
2020-04-01 01:45:07 +0100

    * README updated

--- TicketNow-Server/src/model/tickets.py
+++ TicketNow-Server/src/model/tickets.py
@@ -1,17 +1,18 @@
-from app import db
+from common.app_init import db
 
 class Ticket(db.Model):
     __tablename__ = "User"
     
     id_ticket = db.Column(db.Integer(10), primary_key=True)
-    price = db.Float
+    price = db.Column(db.Float(),nullable=False)
+
 
     email = db.Column(db.String(320))
     password_hash = db.Column(db.String(200))
     name = db.Column(db.String(100))
 
     def get_user(id_user):
-        return Users.query.filter_by(id_user=id_user).first()
+        return User.query.filter_by(id_user=id_user).first()
 
 
 
