b76566cf3a736eab5665963cb1e5370ca842af14
KillByte <killbyte.games@gmail.com>
2020-04-30 14:07:22 +0100

    + Dev tokens auth override

--- TicketNow-Server/src/common/app_init.py
+++ TicketNow-Server/src/common/app_init.py
@@ -21,10 +21,16 @@ app.config['BASE_ENDPOINT'] = '/api'
 
 db = SQLAlchemy(app)
 
+dev_tokens_whitelist = [ 
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1ODgyNTE1ODAsIm5iZiI6MTU4ODI1MTU4MCwianRpIjoiMzViMTk5NjgtMTg1MS00Y2JiLTkxN2UtNjczNWUzYTkzNDQ1IiwiZXhwIjoxNjE5Nzg3NTgwLCJpZGVudGl0eSI6ImExMjM0NSIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyIsInVzZXJfY2xhaW1zIjp7InBlcm1pc3Npb25zIjo1fX0._-ZUM1O5Iy6f-N8f7WjCti8n0JGIfH6jt4ejpdvwBmQ",
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1ODgyNTE0MzMsIm5iZiI6MTU4ODI1MTQzMywianRpIjoiMjA0YmY2OGMtNDZmYy00NzUxLThhY2MtNzMwOGZjN2FkNGNiIiwiZXhwIjoxNjE5Nzg3NDMzLCJpZGVudGl0eSI6ImExMTExMSIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyIsInVzZXJfY2xhaW1zIjp7InBlcm1pc3Npb25zIjoxfX0.1HPkWdG55joGPv6_fBZYXXmVa57An32csYHSxYYM0ZU"
+    ]
+
+
 # Reuse of token blacklist for token-session validattion
-#@jwt.token_in_blacklist_loader
-#def check_if_token_in_blacklist(decoded_token):
-#    return not SessionTable.contains(decoded_token['identity'],decoded_token['jti'])
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decoded_token):
+    return not SessionTable.contains(decoded_token['identity'],decoded_token['jti'])
 
 
 
